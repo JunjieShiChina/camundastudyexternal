7b24e36e0f9de06ffadc614ebb3144bb36eb70b1
shijunjiee <shijunjiee@enn.cn>
2024-04-11 10:53:18 +0800

    获取扩展属性测试

--- src/main/java/me/shijunjiee/camundastudyexternal/external/service/DispatchTaskHandler.java
+++ src/main/java/me/shijunjiee/camundastudyexternal/external/service/DispatchTaskHandler.java
@@ -9,13 +9,19 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-@ExternalTaskSubscription(topicName = "dispatchTask", processDefinitionKeyIn = {"process_aysc"}, lockDuration = 20000)
+@ExternalTaskSubscription(topicName = "dispatchTask", processDefinitionKeyIn = {"process_aysc"}, includeExtensionProperties = true, lockDuration = 20000)
 public class DispatchTaskHandler implements ExternalTaskHandler {
+
     @Override
     public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
         System.out.println("Dispatch task handler");
         Map<String, Object> allVariables = externalTask.getAllVariables();
         allVariables.forEach((k, v) -> System.out.println(k + " : " + v));
+
+        // 获取扩展属性
+        Map<String, String> extensionProperties = externalTask.getExtensionProperties();
+        extensionProperties.forEach((k, v) -> System.out.println(k + " : " + v));
+
         externalTaskService.complete(externalTask);
     }
 }
